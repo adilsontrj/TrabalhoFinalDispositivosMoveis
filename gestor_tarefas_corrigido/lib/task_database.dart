bf5c9d8e34aea653e2f6c91f0dd2fd3ba7a215fb
adilsontrj <adilsonjuniorrocha123@gmail.com>
2025-07-07 21:56:43 -0300

    Trabalho final Finalizado

---
+++ gestor_tarefas_corrigido/lib/task_database.dart
@@ -0,0 +1,55 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'task.dart';
+
+class TaskDatabase {
+  static final TaskDatabase instance = TaskDatabase._init();
+  static Database? _database;
+
+  TaskDatabase._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('tasks.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        description TEXT NOT NULL
+      )
+    ''');
+  }
+
+  Future<Task> create(Task task) async {
+    final db = await instance.database;
+    final id = await db.insert('tasks', task.toMap());
+    return task.copyWith(id: id);
+  }
+
+  Future<List<Task>> readAllTasks() async {
+    final db = await instance.database;
+    final result = await db.query('tasks');
+    return result.map((json) => Task.fromMap(json)).toList();
+  }
+
+  Future<int> delete(int id) async {
+    final db = await instance.database;
+    return await db.delete('tasks', where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future close() async {
+    final db = await instance.database;
+    db.close();
+  }
+}
